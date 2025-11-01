3c834347f75885c0a1256f5c21375a1dbfcee2ca
aDarMar <d.ario27@outlook.it>
2025-11-01 21:46:37 +0100

    add: added linear system solution

--- Functions/plot_results.m
+++ Functions/plot_results.m
@@ -1,37 +1,124 @@
-function plot_results(flg,t,x,x_aux)
+function plot_results(AC,flg,t,x,x_aux,x_debug,tl,xl,A,C,xref)
 %PLOT_RESULTS Function that plots the results of the integration
 %   INPUT
 %   x - longitudinal state vector [V,ga,h,m]
-%   y - longitudinal output vector [VIAS,M,h,hdot]
-%   
+%   x_aux - additional daata taken from ode's OutputFcn [t,ID,Vc,hdotc,Kc,CL,T]
+%   tl,xl - solution vectors for the linear case
     switch flg
         case 'ctrl'
         % Commanded valeu case
-        i = 1;                                          % figure index
-        y = LongDynNoLin_Out(x'); y = y';                        % output vector
-        fig(i) = figure('Name','1');
-        % fig 1: hdot,V,CL,T
-        plotvec = [y(:,1),y(:,4),x_aux(:,5:6),x_aux(:,2:3)];      % Vector used to plot results [VIAS,hdot,CL,T,Vc,hdotc]
-        nCase = length(plotvec(1,:)) - 2;               % Number of subplots
-        TIT = {'Actual vs Commanded IAS','Actual vs Commanded hdot','CL','T'};
-        for j = 1:2
-            ax(i,j) = subplot(nCase,1,j,'Parent',fig(i));
-            hold(ax(i,j),'on'); title(ax(i,j),TIT{j});
-            plot( t,plotvec(:,j) );
-            plot( t,plotvec(:,nCase+j),'--r' );
-        end
-        for j = 3:nCase
-            ax(i,j) = subplot(nCase,1,j,'Parent',fig(i));
-            hold(ax(i,j),'on'); title(ax(i,j),TIT{j});
-            plot( t,plotvec(:,j) );
-
-        end
-
-
-
+            plotfigs(AC,t,x,x_aux,x_debug);
+        
+        case 'cfr'
+            % Confronts linear and nonlinear cases
+            [fig,ax] = plotfigs(AC,t,x,x_aux,x_debug);
+            i = 1;
+            y = C*xl'; y = y';
+            yref = LongDynNoLin_Out(xref'); yref = yref';                        % output vector
+            plotvec = [y(:,1)+yref(1),y(:,4)+yref(4),...
+                xl(:,4)+xref(end-1),xl(:,5)+xref(end)];      % Vector used to plot results [VIAS,hdot,CL,T,Vc,hdotc]
+            nCase = 4;
+            for j = 1:nCase
+                ax(i,j) = subplot(nCase,1,j,'Parent',fig(i));
+                hold(ax(i,j),'on');
+                plot( ax(i,j),tl,plotvec(:,j),'.-b' );
+                %plot( ax(i,j),t,plotvec(:,nCase+j),'--r' );
+            end
+            i = 2; nCase = 3;
+            for j = 1:nCase
+                plot( ax(i,j),tl,xl(:,j)+xref(j),'.-b'  );
+            end
+            % Plots the roots
+            lam = eig(A);
+            i = 3; fig(i) = figure('Name','Linearized System Roots');
+            j = 3; ax(i,j) = axes('Parent',fig(i));
+            plot( ax(i,j),real(lam),imag(lam),'diamond','MarkerSize', 8, ...            % dimensione del marker
+            'MarkerEdgeColor', [0 0 0], ...% colore bordo (nero)
+            'MarkerFaceColor', [0 0.5 0], ...% colore interno (rosso)
+            'LineWidth', 1.5);
         case 'waypoint'
-    
+        
+        
     end
 
 end
 
+function [fig,ax] = plotfigs(AC,t,x,x_aux,x_debug)
+    % Output Variables and Commanded Variables
+    i = 1;                                          % figure index
+    y = LongDynNoLin_Out(x'); y = y';                        % output vector
+    fig(i) = figure('Name','Output and Commanded Variables');
+    % fig 1: hdot,V,CL,T
+    plotvec = [y(:,1),y(:,4),x_aux(:,6:7),x_aux(:,3:4)];      % Vector used to plot results [VIAS,hdot,CL,T,Vc,hdotc]
+    nCase = length(plotvec(1,:)) - 2;               % Number of subplots
+    TIT = {'Actual vs Commanded IAS','Actual vs Commanded hdot','CL','T'};
+    for j = 1:2
+        ax(i,j) = subplot(nCase,1,j,'Parent',fig(i));
+        hold(ax(i,j),'on'); title(ax(i,j),TIT{j});
+        plot( ax(i,j),t,plotvec(:,j) );
+        plot( ax(i,j),t,plotvec(:,nCase+j),'--r' );
+    end
+    for j = 3:nCase
+        ax(i,j) = subplot(nCase,1,j,'Parent',fig(i));
+        hold(ax(i,j),'on'); title(ax(i,j),TIT{j});
+        plot( ax(i,j),t,plotvec(:,j) );
+        if nargin > 4
+            plot( ax(i,j),x_debug(:,1),x_debug(:,3+j),'--m' );
+        end
+
+    end
+    % State Variables
+    i = 2;  fig(i) = figure('Name','1');
+    fig(i) = figure('Name','State Variables');
+    nCase = 5; TIT = {'V','\gamma_a','h','m','I_c'};
+    for j = 1:nCase
+        ax(i,j) = subplot(nCase,1,j,'Parent',fig(i));
+        hold(ax(i,j),'on'); title(ax(i,j),TIT{j});
+        plot( t,x(:,j) );
+
+    end
+    % Forces
+    i = 3; j = 1;
+    fig(i) = figure('Name','Forces');
+    qs = t(:)*nan; L = qs; D = qs; W = [qs,qs]; Re = qs;
+    for in = 1:length(x(:,1))
+        [T, a, P, rho] = atmosisa(x(in,3));
+        qs(in) = 0.5*rho(:)*x(in,1).^2; Re(in) = AC.ReCalc(x(in,3),y(in,2));
+        D(in) = qs(in)*AC.Sw*AC.polar(y(in,2),Re(in),x_aux(in,6));
+        L(in) = qs(in)*AC.Sw*x_aux(in,6);
+        W(in,1) = 9.81*x(in,4)*sin(x(in,2));
+        W(in,2) = 9.81*x(in,4)*cos(x(in,2));
+    end
+    plotvec = [L,D,W,x_aux(:,7)]; nCase = length(plotvec(1,:));
+    ax(i,j) = subplot(2,1,j,'Parent',fig(i) ); hold(ax(i,j),'on');
+    %ax(i,j) = axes('Parent',fig(i));  title(ax(i,j),'Forces');
+    grid(ax(i,j),'minor');
+    colors = [
+        0,    0,    1;    % Blu scuro
+        1,    0,    0;    % Rosso
+        0.5,  1,    0;    % Verde lime
+        0,    1,    1;    % Ciano
+        1,    0,    1;    % Magenta
+        1,    0.647, 0;   % Arancione
+        1,    1,    0     % Giallo
+        ];
+    NM = {'L','D','Wsin(\gamma)','Wcos(\gamma)','T'};
+    for k = 1:nCase
+        %ax(i,1) = subplot(nCase,1,j,'Parent',fig(i));
+        plot( ax(i,j),t,plotvec(:,k), 'LineWidth', 1.2,  'Color', ...
+            colors(k,:), 'LineStyle', '-', ...
+            'DisplayName', NM{k} );
+    end
+    
+    legend(ax(i,1));
+    nCase = 2; j = 2; NM = {'Re*1e7','M'};
+    ax(i,j) = subplot(2,1,j,'Parent',fig(i) ); hold(ax(i,j),'on');
+    plotvec = [Re*1e-7,y(:,2)];
+    for k = 1:nCase
+        %ax(i,1) = subplot(nCase,1,j,'Parent',fig(i));
+        plot( ax(i,j),t,plotvec(:,k), 'LineWidth', 1.2,  'Color', ...
+            colors(k,:), 'LineStyle', '-', ...
+            'DisplayName', NM{k} );
+    end
+    legend(ax(i,j));
+end
--- longitudinal_setup.m
+++ longitudinal_setup.m
@@ -9,6 +9,7 @@ main()
 % We define main function to allow the nested functions definition to store
 % additional data through the integration
 function main()  
+    CfrLin = true;
     %% Aircraft Definition
     nmfl = "Q100.aero"; AC = ACclass(nmfl);
     
@@ -30,7 +31,7 @@ function main()
     %x_add = storefun(addata);
     addata = nan(1,6);         % ID,Vc,hdotc,Kh,CL,T
     out_store = storefun([nan,addata]); % CONTROLLARE SE FUNZIONANO E INIZIALIZZARE
-    out_step = storefun(addata);
+    out_step = storefun([nan,addata]);
     %% ODE RHS definition
     
     function dxdt = LongDynNLConSimp(t,x,AC,y_way)
@@ -42,13 +43,19 @@ function main()
             addt(2) = t;
         end
 
-        [uct,~,dxdt_c] = LongControlOutSimple(t,x,y_way,addt,AC);
+        [uct,uout,dxdt_c] = LongControlOutSimple(t,x,y_way,addt,AC);
         dxdt = LonDynNoLin(t,x,uct,AC);
         addt(1) = t;
         x_add = storefun(addt);
-        %out_stored = out_store();
-        %out_store = storefun([out_stored;[t,uout(:)',uct(:)']]); % CREARE UN VETTORE DI OUTPUT DA SALVARE
+        out_step = out_step();
+        out_step = storefun([out_step;[t,uout(:)',uct(:)']]); % CREARE UN VETTORE DI OUTPUT DA SALVARE
         dxdt = [dxdt(:);dxdt_c(:)];
+    end
+    
+    function dxdt = LongDynLinCon(t,x)
+       
+       dxdt =  LongDynLin(t,x,Acl,Bcl,y_way);
+        
     end
 
     function dxdt = LongDynNL_cont(t,x,AC,y_way,bounds)
@@ -108,10 +115,10 @@ function main()
             % Main code to update param
                 nt = length(t);
                 store_temp = nan(nt,7); % [t,ID,Vc,hdotc,Kc,CL,T]
-                store = out_store();
+                store = out_store(); addt = x_add();
                 for it = 1 :nt
                     [uct,uout,~] = LongControlOutSimple(t,x,y_way,addt,AC);
-                    store_temp(it,1) = t;
+                    store_temp(it,1) = t(it);
                     store_temp(it,2:5) = uout(:)'; store_temp(it,6:7) = uct(:)';
                 end
                 out_store = storefun([store;store_temp]); 
@@ -124,24 +131,66 @@ function main()
     end
 
     %% Simulation
-    CHS = 'ctrl';
-    options = odeset('RelTol',1e-5,'AbsTol',1e-4);%,'OutputFcn',@myout);%,...
+    CHS = 'ctrl'; 
+    options = odeset('RelTol',1e-6,'AbsTol',1e-5);%,'OutputFcn',@myout);%,...
    %'Events',@events);
-    x_way = [ 728/3.3; 1; 25000/3.3]; % Vias [kts] h [ft] hdot [ft/min]
-    switch CHS
-        case 'ctrl'
-            Tfin = 40;
-            hdotc = 1000;               % ft/min RoC
-            load('Data\test_conditions.mat','xref','x0');
-            options.OutputFcn = @myoutSimple;
-            te = 0; x_add = storefun([te;te;hdotc]);
-            ye = xref(1:4)'; ye = [ye;0;0];
-            y_way = LongDynNoLin_Out( xref(:) );      % Defining the y desired output
-
-            [t,x] = ode45( @(t,x)LongDynNLConSimp( t,x,AC,y_way ),...
+   x_way = [ 728/3.3; 1; 25000/3.3]; % Vias [kts] h [ft] hdot [ft/min]
+   switch CHS
+       case 'ctrl'
+           CTR = 'Vc'; % Cambia qua e la variabile TEST in LongControlOutSimple
+           
+           Tfin = 40;
+           
+           
+           load('Data\test_conditions.mat','xref','x0');
+           options.OutputFcn = @myoutSimple;
+           % Nonlinear System
+           te = 0; 
+           xref = x0(36,:);
+           ye = xref(1:4)'; ye = [ye;0;0];
+           y_way = LongDynNoLin_Out( xref(:) );      % Defining the y desired output
+           y_way = zeros(4,1);
+           
+           switch CTR
+               case 'hc'
+                   hdotc = 1000;               % ft/min RoC
+                   y_way(4) = hdotc;
+                   x_add = storefun([te;te;hdotc]);
+                   k = 1;
+               case 'Vc'
+                   Vc = 200;                   % kts IAS
+                   y_way(1) = Vc;
+                   x_add = storefun([te;te;Vc]);
+                   k = 2;
+               case'b'
+                   Vc = 200;
+                   hdotc = 100;
+                   y_way(1) = Vc;
+                   y_way(4) = hdotc;
+                   x_add = storefun([te;te;Vc]);
+                   k = 3;
+           end
+            
+            
+            [t,x] = ode113( @(t,x)LongDynNLConSimp( t,x,AC,y_way ),...
                                 [te,Tfin],ye,options );
-            x_aux = out_store(); x_aux = x_aux(2:end,:);
-            plot_results(CHS,t,x,x_aux);
+            x_aux = out_store(); %x_aux = x_aux(2:end,:);       % Left the first nan column because the @myout function is called from the first successfull timestep after t0
+            x_debug = out_step();
+            
+            % Linearized System
+            if CfrLin
+                % Compares the linear and non linear responses
+                [A,B,C,u0] = AC.LongLinSys( ye,ye(4) );
+                A = [A,B;zeros(2,5)]; B = [B,zeros(3,2);zeros(2,2),eye(2)];
+                C = [C,zeros(4,2)];
+                Acl = A - B*[AC.Kp(:,:,k)+AC.Kb(:,:,k);AC.Ki(:,:,k)]*C;
+                Bcl = B*[AC.Kp(:,:,k);AC.Ki(:,:,k)];
+                [tl,xl] = ode45( @LongDynLinCon,[0,Tfin],...
+                    zeros(5,1) );
+                plot_results(AC,'cfr',t,x,x_aux,x_debug,tl,xl,Acl,C,[ye',u0']);
+            else
+                plot_results(AC,CHS,t,x,x_aux,x_debug);
+            end
         case 'waypoint'
             te = 0;                                              % Starting time
             ye = x_way(:,1);                                     % Initial condition
